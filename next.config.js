49935fa09271db603041c69210e3852d0a4770d7
Alex Pedersen <mails@alexpedersen.dev>
2022-10-11 18:16:51 +0200

    fix(cors): add graphql url to rewrites

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   ignoreDuringBuilds: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/graphql/:path*',
+        destination: `https://graphql-gateway.axieinfinity.com/graphql/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
